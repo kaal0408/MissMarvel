4922ac3c817867fe96ff269ae8a44f8fd06db05d
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2021-11-28 10:06:11 +0530

    Update __main__.py

--- marvel/__main__.py
+++ marvel/__main__.py
@@ -126,7 +126,7 @@ RANDOM_START = (
     "Why you awaked meh?",
     "Hello there how can i help you?",
     "Arey you human?",
-    "Hey I'm Coded by @noobanon",
+    "Hey I'm Coded by @Hayat_Murat_30",
     "Are you alive ? Umm I think yes",
     "Are you stalking meh ?",
 	"Relax I'm here",
@@ -173,7 +173,7 @@ def start(update, context):
             buttons = InlineKeyboardMarkup( 
                 [[InlineKeyboardButton(text="🎉 Add Me", url="t.me/YukoAraki_bot?startgroup=botstart"), InlineKeyboardButton(text="❓ Help", callback_data="help_back")],
                 [InlineKeyboardButton(text="👥 Support Group", url="https://t.me/Pikachux_support")],
-                [InlineKeyboardButton(text="Repo", url="https"https://t.me/Pikachux_support)]])
+                [InlineKeyboardButton(text="Repo", url="https"https://t.me/Pikachux_support")]])
             update.effective_message.reply_text(
                 tld(update.effective_message, PM_START_TEXT).format(escape_markdown(first_name), escape_markdown(context.bot.first_name), OWNER_ID),
                 disable_web_page_preview=True,
