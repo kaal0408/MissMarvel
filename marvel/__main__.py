e10d5c705e72e4781d4e949cb25e0edb346a6ff9
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2021-11-28 10:26:35 +0530

    Update __main__.py

--- marvel/__main__.py
+++ marvel/__main__.py
@@ -197,46 +197,7 @@ def start(update, context):
     # empty string works fine.
     #  trace = "".join(traceback.format_tb(sys.exc_info()[2]))
     # lets try to get as much information from the telegram update as possible
-    payload = ""
-    # normally, we always have an user. If not, its either a channel or a poll update.
-    if update.effective_user:
-        payload += f' with the user {mention_html(update.effective_user.id, update.effective_user.first_name)}'
-    # there are more situations when you don't get a chat
-    if update.effective_chat:
-        payload += f' within the chat <i>{update.effective_chat.title}</i>'
-        if update.effective_chat.username:
-            payload += f' (@{update.effective_chat.username})'
-    # but only one where you have an empty payload by now: A poll (buuuh)
-    if update.poll:
-        payload += f' with the poll id {update.poll.id}.'
-    # lets put this in a "well" formatted text
-    text = f"Hey.\n The error <code>{context.error}</code> happened{payload}. The full traceback:\n\n<code>{trace}" \
-           f"</code>"
-    # and send it to the dev(s)
-    for dev_id in devs:
-        context.bot.send_message(dev_id, text, parse_mode=ParseMode.HTML)
-    # we raise the error again, so the logger module catches it. If you don't use the logger module, use it.
-    try:
-        raise context.error
-    except Unauthorized:
-        # remove update.message.chat_id from conversation list
-        LOGGER.exception('Update "%s" caused error "%s"', update, context.error)
-    except BadRequest:
-        # handle malformed requests - read more below!
-        LOGGER.exception('Update "%s" caused error "%s"', update, context.error)
-    except TimedOut:
-        # handle slow connection problems
-        LOGGER.exception('Update "%s" caused error "%s"', update, context.error)
-    except NetworkError:
-        # handle other connection problems
-        LOGGER.exception('Update "%s" caused error "%s"', update, context.error)
-    except ChatMigrated as e:
-        # the chat_id of a group has changed, use e.new_chat_id instead
-        LOGGER.exception('Update "%s" caused error "%s"', update, context.error)
-    except TelegramError:
-        # handle all other telegram related errors
-        LOGGER.exception('Update "%s" caused error "%s"', update, context.error)
-
+    
 
 def help_button(update, context):
     query = update.callback_query
