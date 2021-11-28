ba35396621ffc7e99b484c5189bb55e9bb8013b3
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2021-11-28 10:43:11 +0530

    Update __main__.py

--- marvel/__main__.py
+++ marvel/__main__.py
@@ -206,27 +206,4 @@ def start(update, context):
 
     
 
-    # dispatcher.add_error_handler(error_callback)
-
-    if WEBHOOK:
-        LOGGER.info("Using webhooks.")
-        updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
-
-        if CERT_PATH:
-            updater.bot.set_webhook(url=URL + TOKEN, certificate=open(CERT_PATH, "rb"))
-        else:
-            updater.bot.set_webhook(url=URL + TOKEN)
-            client.run_until_disconnected()
-
-    else:
-        LOGGER.info("Using long polling.")
-        updater.start_polling(timeout=15, read_latency=4)
-        client.run_until_disconnected()
-
-    updater.idle()
-
-
-if __name__ == "__main__":
-    LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
-    client.start(bot_token=TOKEN)
-    main()
+    
